2d8f0d859147bb7747e205508ca7a6787d0751d4
ShireenBean21 <124299943+ShireenBean21@users.noreply.github.com>
2023-04-02 19:30:49 +0100

    Add files via upload

--- exercise17/bankaccount.py
+++ exercise17/bankaccount.py
@@ -3,18 +3,17 @@ class InsufficientFundsException(Exception):
 
 
 class BankAccount:
-    def __init__(self, acc_no, balance=0):
-        self.acc_no = acc_no
+    def __init__(self, account_number, balance=0):
+        self.account_number = account_number
         self.balance = balance
+
+    def __str__(self):
+        return f"Account number: {self.account_number}, Balance: {self.balance}"
 
     def deposit(self, amount):
         self.balance += amount
 
     def withdraw(self, amount):
-        if self.balance >= amount:
-            self.balance -= amount
-        else:
-            raise InsufficientFundsException("Unable to complete transaction: Insufficient Funds")
-
-
-        return f"{type(self).__name__}({self.acc_no}, {self.balance})
+        if self.balance < amount:
+            raise InsufficientFundsException("Unable to complete transaction: Insufficient funds")
+        self.balance -= amount
--- exercise17/customer.py
+++ exercise17/customer.py
@@ -1,9 +1,9 @@
-from bankaccount import BankAccount
-from bankaccount import InsufficientFundsException
-
+from bankaccount import BankAccount, InsufficientFundsException
+# create two bank accounts
 account1 = BankAccount("11111", balance=500)
 account2 = BankAccount("33333", balance=1000)
 
+# try to withdraw money from account1
 try:
     account1.withdraw(1000)
     print(account1)
@@ -11,4 +11,9 @@ except InsufficientFundsException as exception:
     print(exception)
     print(account1)
 
-print(account2)
+try:
+    account2.withdraw(600)
+    print(f"Withdrawal successful Your Account balance is now £{account2.balance}")
+except InsufficientFundsException as exception:
+    print(exception)
+
