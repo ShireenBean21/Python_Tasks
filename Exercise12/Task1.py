6726123c4a4bb94e1b5824954a804e89cd2c2271
ShireenBean21 <124299943+ShireenBean21@users.noreply.github.com>
2023-04-02 01:28:47 +0100

    Add files via upload

---
+++ Exercise12/Task1.py
@@ -0,0 +1,8 @@
+cheese = ['Cheddar', 'Stilton', 'Cornish Yarg']
+cheese.append('Oke')
+
+cheese.extend(['brie', 'Edam'])
+
+print(cheese)
+
+#Task 2: The first is a string so the output counts the number of characters the second is a tuple.
---
+++ Exercise12/lotto.py
@@ -0,0 +1,3 @@
+import random
+winning_numbers = random.sample(range(1, 51), 6)
+print(f"The winning numbers are: ", (winning_numbers))
